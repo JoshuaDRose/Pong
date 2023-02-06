436c4fe0be6a1a7292c332ca7c47ac72ae2d090d
ExtremeYeti1267 <124556890+ExtremeYeti1267@users.noreply.github.com>
2023-02-06 18:41:13 +1100

    Update Paddle.py

--- Paddle.py
+++ Paddle.py
@@ -1,1 +1,25 @@
+import pygame
 
+
+class Paddle(pygame.sprite.Sprite):
+        def __init__(self, x: int, y: int):
+                """
+                x: x position on screen
+                y: y position on screen
+                """
+
+                super().__init__()
+                self.image = pygame.Surface([50, 50])
+                self.rect = self.image.get_rect()
+                
+                self.color = (255, 0, 0)
+                pygame.draw.rect(self.image, self.color (x, y), radius)
+                
+                """
+                direction
+                ---------
+                if x and y are both positive: moving towards bottom right
+                x is positive but y is negative: moving towards top right
+                x is negative and y is negative: moving top left
+                x is negative and y is positive: moving bottom left
+                """
