f72fe674056d36a9672a5e2a9b750688fdd260b0
= <joshuarose099@gmail.com>
2023-02-06 19:20:18 +1100

    Added ball and drew it to screen and indent stuff

--- main.py
+++ main.py
@@ -29,25 +29,32 @@ ball = Ball(width//2, height//2, 15)
 
 # Start game-loop
 while running:
-	screen.fill(background_color)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-			
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_w:
-				print ("A pressed")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-			elif event.key == pygame.K_s:
-				print ("S pressed")
-				
-			if event.key == pygame.K_UP:
-				print ("Up pressed")
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print ("A pressed")
 
-			elif event.key == pygame.K_DOWN:
-				print ("Down pressed")
-			
-	# Update the display
-	pygame.display.update()
+            elif event.key == pygame.K_s:
+                print ("S pressed")
+
+            if event.key == pygame.K_UP:
+                print ("Up pressed")
+
+            elif event.key == pygame.K_DOWN:
+                print ("Down pressed")
+
+
+    # fill the screen
+    screen.fill(background_color)
+
+    ball.draw(screen)
+    ball.update()
+
+    # Update the display
+    pygame.display.update()
+
 pygame.quit()
 sys.exit()
