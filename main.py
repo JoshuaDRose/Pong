4b9ba72761564116d5f6144e3c348657ce03974e
ExtremeYeti1267 <124556890+ExtremeYeti1267@users.noreply.github.com>
2023-02-06 18:11:49 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import pygame
+import pygame, sys
 
-width = 800
-height = 900
-
+#Initiating Pygame
 pygame.init()
 
+#Window dimensions
+WinSize = (700,400)
+
+#Creating a display
+pygame.display.set_mode(WinSize)
+
+#Events loop.
+while True:
+	for event in pygame.event.get():
+		
+		#Quits Pygame
+		if event.type == pygame.QUIT:
+			print ("(:-))-|---<")
+			pygame.quit()
+			sys.exit()
